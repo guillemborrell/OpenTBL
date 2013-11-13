7a32af9d6b1b2859bcf93cc8f18067137e11b0b0
Guillem Borrell <guillem@torroja.dmt.upm.es>
2013-11-13 01:30:47 +0100

    Version staged before sending the tarball to Callum

--- genflu.F90
+++ genflu.F90
@@ -53,6 +53,7 @@ subroutine genflu(ut,vt,wt,y,re,dt,tiempo,mpiid,m,communicator)
      
 
      do j=1,ny+1
+        !Guillem, 11-11-13. Why the fuck is this 119 hardcoded here?
       fblend=0.5*(1-tanh(y(j)-y(119)))
       ut(2:,j,1)=ut(2:,j,1)*fblend
       wt(2:,j,1)=wt(2:,j,1)*fblend
